9d2cf5225b2e10949bad7e66432741470a751d6d
YahArt <hutteryannick@gmail.com>
2021-02-28 15:28:07 +0100

    Implement basic jumping

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
 GREEN_COLOR = (159, 188, 77)
 CACTUS_SPAWN_RATE = 3000
+CACTUS_MOVEMENT_SPEED = -0.3
+JUMP_HEIGHT = 150
+JUMP_FORCE = 0.6
 
 class Sprite():
 	def __init__(self, x, y, image):
@@ -72,6 +75,49 @@ class ParallaxSprite(Sprite):
 	def update(self, dt):
 		self.x += self.horizontal_speed * dt
 
+class Dinosaur():
+
+	def __init__(self, x, y):
+		self.sprite = AnimatedSprite(x, y, pg.image.load(os.path.join("assets", "dinosaur_green.png")), 72, 72)
+		self.sprite.add_animation("run", [17, 18, 19, 20], 125)
+		self.sprite.set_current_animation("run")
+		self.max_jump_height = self.sprite.y - JUMP_HEIGHT
+		self.ground_pos = self.sprite.y
+		self.is_grounded = True
+		self.is_jumping = False
+		self.is_falling = False
+
+	def update(self, dt):
+		# When we jump we actually need to subtract from your y position as the top left is 0/0
+		if self.is_jumping and self.sprite.y > self.max_jump_height:
+			self.sprite.y -= JUMP_FORCE * dt
+
+		# Once we have reached our max jump height we fall
+		elif self.sprite.y < self.max_jump_height and self.is_jumping:
+			self.is_falling = True
+			self.is_jumping = False
+
+		# Fall for as long as we are not at our original ground position
+		if self.is_falling and self.sprite.y < self.ground_pos:
+			self.sprite.y += JUMP_FORCE * dt
+
+		# Once we have reached our ground position stop falling
+		if self.sprite.y >= self.ground_pos and self.is_falling:
+			self.is_falling = False
+			self.is_grounded = True
+			self.sprite.y = self.ground_pos
+
+		self.sprite.update(dt)
+
+	def jump(self):
+		# Only allow jumping when grounded
+		if self.is_grounded:
+			self.is_grounded = False
+			self.is_jumping = True
+
+	def draw(self, window):
+		self.sprite.draw(window)
+
 
 def run():
 	# Setup PyGame
@@ -85,11 +131,9 @@ def run():
 	font = pg.font.Font(os.path.join("assets", "game_font.ttf"), 16)
 
 	# Setup objects
-	dinosaur = AnimatedSprite(0, 260, pg.image.load(os.path.join("assets", "dinosaur_green.png")), 72, 72)
-	dinosaur.add_animation("run", [17, 18, 19, 20], 125)
-	dinosaur.set_current_animation("run")
+	dinosaur = Dinosaur(0, 260)
 
-	cacti = [ParallaxSprite(round(WINDOW_WIDTH * 0.8), 250, pg.image.load(os.path.join("assets", "cactus_green.png")), -0.1)]
+	cacti = [ParallaxSprite(round(WINDOW_WIDTH * 0.8), 250, pg.image.load(os.path.join("assets", "cactus_green.png")), CACTUS_MOVEMENT_SPEED)]
 	floor = ParallaxSprite(0, 300, pg.image.load(os.path.join("assets", "floor_green.png")), -0.1)
 	floor_second = ParallaxSprite(floor.width(), 300, pg.image.load(os.path.join("assets", "floor_green.png")), -0.1)
 
@@ -108,7 +152,7 @@ def run():
 		# Check if we need to spawn a new cactus
 		if spawn_timer > CACTUS_SPAWN_RATE:
 			spawn_timer = 0
-			cacti.append(ParallaxSprite(WINDOW_WIDTH, 250, pg.image.load(os.path.join("assets", "cactus_green.png")), -0.1))
+			cacti.append(ParallaxSprite(WINDOW_WIDTH, 250, pg.image.load(os.path.join("assets", "cactus_green.png")), CACTUS_MOVEMENT_SPEED))
 
 		# Update objects
 		cacti_to_delete = []
@@ -135,6 +179,8 @@ def run():
 		for event in pg.event.get():
 			if event.type == pg.QUIT:
 				running = False
+			if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+				dinosaur.jump()
 
 		# Draw
 		window.fill((255, 255, 255))
